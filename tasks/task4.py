aeb9ebbbab724d80f94e064d47b8816c66de8dfc
Joshua <jschmidtnj@gmail.com>
2019-10-23 10:21:19 -0400

    added some get_coords math

--- tasks/task4.py
+++ tasks/task4.py
@@ -17,6 +17,9 @@ from contest import Run
 def pose_from_info(x, y, angle=0):
     return orspy.EulerPose(x, y, 0, 0, 0, angle)
 
+def avg(arr):
+    return sum(arr) / len(arr)
+
 
 class State:
     def __init__(self, sched, simulated, run_number, try_legit, auto_submit_minutes):
@@ -26,7 +29,7 @@ class State:
             pose_from_info(66.5, 96.5),
             pose_from_info(66.5, 95.5, 0.01),
             pose_from_info(66.5, 98.5, 0.01),
-            pose_from_info(69.5, 98.5, 3*math.pi/2),
+            pose_from_info(69.1, 98.5, 3*math.pi/2),
             pose_from_info(68.5, 101.5, math.pi),
             pose_from_info(66, 105.5),
             pose_from_info(68.5, 105.5)]#,
@@ -43,7 +46,7 @@ class State:
             pose_from_info(65, 102, 0.01),
             pose_from_info(65, 97)
         '''
-        self.object_data = {
+        self.given_object_data = {
             "tablet": {
                 "num_object": 1,
                 "num_submit": 0
@@ -114,8 +117,7 @@ class State:
         sched.use_simulated(simulated)
         self.poses = []
         self.images = []
-        self.objects = []
-        self.last_timestamp = 0
+        self.objects = {}
         self.run_number = 1
         self.try_legit = try_legit
         self.start_time = time.time()
@@ -126,7 +128,7 @@ class State:
 
     def navigate_to(self, location):
         return self.sched.goto(location,
-                               allowed_trans_error=0.5, allowed_rot_error=6.if location.c == 0 else 0.6,
+                               allowed_trans_error=0.75, allowed_rot_error=6.if location.c == 0 else 0.6,
                                maxspeed=0.3)
 
     def rotate(self):
@@ -138,7 +140,6 @@ class State:
         self.topic_pose = "{}/object_pose".format(self.sched.wws_topic_prefix())
         wws_lite.subscribe(self.topic_img, lambda x: self.wws_detect_img_cb(x))
         wws_lite.subscribe(self.topic_pose, lambda x: self.detect_cb_pose(x))
-        self.sched.stop_action(-1)
 
     def clear_action(self):
         self.sched.stop_action(self.action)
@@ -146,87 +147,95 @@ class State:
     def end(self):
         wws_lite.unsubscribe(self.topic_img)
         wws_lite.unsubscribe(self.topic_pose)
+        self.sched.stop_action(-1)
 
-    def get_size_data(self, m):
+    def get_size_data(self, m, index):
         bbox = None
         if self.simulated:
-            bbox = m['object']['payload']['objects']['detections'][0]['bbox']
+            bbox = m['object']['payload']['objects']['detections'][index]['bbox']
         else:
-            bbox = m['object']['payload']['detections'][0]['bbox']
+            bbox = m['object']['payload']['detections'][index]['bbox']
         # y_top, x_left, y_bot, x_right = bbox
         return bbox
 
-    def detect_cb_pose(self, m):
+    def get_coordinates(self, robot_x, robot_y, robot_angle, bbox, size):
+        fudge_factor = 1.7 # meters away for each object (as we pass by)
+        camera_offset_front = .242
+        field_of_view = 1.0472 # radians
+        pixels_wide = 640 # pixels
+        pixels_tall = 480
+        focal_length = 0.001933 # meters
+        average_location_box = avg([bbox[1], bbox[3]])
+        angle_from_robot = ((robot_angle + 1) / 2. * math.pi * 2) - field_of_view / 2. + average_location_box * field_of_view
+        x_location = robot_x + math.sin(angle_from_robot) * fudge_factor
+        y_location = robot_y + math.cos(angle_from_robot) * fudge_factor
+        return x_location, y_location
+
+    def save_object(self, class_name, m, x, y, angle):
         delta_same_object = 5
-        fudge_factor = 5
+        bbox = self.get_size_data(m, i)
+        size = abs(bbox[1] - bbox[3])
+        x, y = get_coordinates(x, y, angle, bbox, size)
+        if class_name in self.objects:
+            if m['timestamp'] - self.objects[class_name]["last_timestamp"] > delta_same_object:
+                # add new object
+                self.objects[class_name].append({
+                        "x": x,
+                        "y": y,
+                        "size": size,
+                        "last_timestamp": m['timestamp'],
+                        "first_index": len(self.images),
+                        "last_index": len(self.images),
+                        "count": 1
+                    })  # new object
+            else:
+                # same object as before
+                if size > self.objects[class_name][-1]["size"]:
+                    self.objects[class_name][-1] = {
+                        "x": x,
+                        "y": y,
+                        "size": size,
+                        "last_timestamp": m['timestamp'],
+                        "first_index": self.objects[-1]["first_index"],
+                        "last_index": len(self.images),
+                        "count": self.objects[-1]["count"] + 1
+                    }  # overwrite current object data
+                else:
+                    self.objects[class_name][-1]['last_timestamp'] = m['timestamp']
+        elif class_name in self.given_object_data:
+            # add to object map
+            self.objects[class_name] = [{
+                "x": x,
+                "y": y,
+                "size": size,
+                "last_timestamp": m['timestamp'],
+                "first_index": len(self.images),
+                "last_index": len(self.images),
+                "count": 1
+            }]  # new object
+
+    def detect_cb_pose(self, m):
         # print(m)
         if self.simulated:
-            if len(m['object']['payload']['objects']['detections']) > 0:
-
+            for i in range(len(m['object']['payload']['objects']['detections'])):
                 # print(m['object']['payload']['objects']['detections'])
-                class_name = m['object']['payload']['objects']['detections'][0]['class_name']
+                class_name = m['object']['payload']['objects']['detections'][i]['class_name']
                 self.poses.append(m)
                 # robot pose
                 print(m)
                 x = m['pose']['payload']['pose']['pose']['position']['x']
                 y = m['pose']['payload']['pose']['pose']['position']['y']
                 angle = m["payload"]["pose"]["orientation"]["z"]
-                bbox = self.get_size_data(m)
-                size = abs(bbox[1] - bbox[3])
-                if m['timestamp'] - self.last_timestamp > delta_same_object:
-                    self.objects.append({
-                        "name": class_name,
-                        "x": x,
-                        "y": y,
-                        "size": size,
-                        "first_index": len(self.images),
-                        "last_index": len(self.images),
-                        "count": 1
-                    })  # new object
-                else:
-                    if size > self.objects[-1]["size"]:
-                        self.objects[-1] = {
-                            "name": class_name,
-                            "x": x,
-                            "y": y,
-                            "size": size,
-                            "first_index": self.objects[-1]["first_index"],
-                            "last_index": len(self.images),
-                            "count": self.objects[-1]["count"] + 1
-                        }  # overwrite current object data
-                self.last_timestamp = m['timestamp']
+                self.save_object(class_name, m, x, y, angle)
         else:
-            if len(m['object']['payload']['detections']) > 0:
-                class_name = m['object']['payload']['detections'][0]['class']['name']
+            for i in range(len(m['object']['payload']['detections'])):
+                class_name = m['object']['payload']['detections'][i]['class']['name']
                 self.poses.append(m)
                 # robot pose
                 x = m['pose']['payload']['pose']['position']['x']
                 y = m['pose']['payload']['pose']['position']['y']
                 angle = m["pose"]["payload"]["pose"]["orientation"]["z"]
-                bbox = self.get_size_data(m)
-                size = abs(bbox[1] - bbox[3])
-                if m['timestamp'] - self.last_timestamp > delta_same_object:
-                    self.objects.append({
-                        "name": class_name,
-                        "x": x,# + math.cos(angle) * size * fudge_factor,
-                        "y": y,# + math.sin(angle) * size * fudge_factor,
-                        "size": size,
-                        "first_index": len(self.images),
-                        "last_index": len(self.images),
-                        "count": 1
-                    })  # new object
-                else:
-                    if size > self.objects[-1]["size"]:
-                        self.objects[-1] = {
-                            "name": class_name,
-                            "x": x,# + math.cos(angle) * size * fudge_factor,
-                            "y": y,# + math.sin(angle) * size * fudge_factor,
-                            "size": size,
-                            "first_index": self.objects[-1]["first_index"],
-                            "last_index": len(self.images),
-                            "count": self.objects[-1]["count"] + 1
-                        }  # overwrite current object data
-                self.last_timestamp = m['timestamp']
+                self.save_object(class_name, m, x, y, angle)
         # print(f"object type- {m['object']['payload']['objects']['detections'][0]['class_name']}")
 
     def wws_detect_img_cb(self, m):
@@ -242,18 +251,17 @@ class State:
     def control_loop(self):
         self.start()
         count = 0
-        for _ in range(1):
-            for way in self.waypoints:
-                if time.time() > self.auto_submit_time:
-                    print("short circuit")
-                    self.submit()
-                    return
-                loc = way
-                self.navigate_to(loc)
-                print(f'saving {len(self.poses)} poses and {len(self.images)} images')  # to data_{count}.pickle' )
-                pickle.dump((self.poses, self.images, self.objects),
-                            open(f'/home/ros/spinachbots/jupyter/data{count}.pickle', 'wb'))
-                count += 1
+        for way in self.waypoints:
+            if time.time() > self.auto_submit_time:
+                print("short circuit")
+                self.submit()
+                return
+            loc = way
+            self.navigate_to(loc)
+            print(f'saving {len(self.poses)} poses and {len(self.images)} images')  # to data_{count}.pickle' )
+            pickle.dump((self.poses, self.images, self.objects),
+                        open(f'/home/ros/spinachbots/jupyter/data{count}.pickle', 'wb'))
+            count += 1
         self.submit()
 
     def submit(self):
@@ -261,27 +269,28 @@ class State:
         min_count = 1
         potential_shipping = ["Priority Mail - USPS", "FedEx"]
         if self.try_legit:
-            for obj in self.objects:
-                if obj["count"] > min_count:
-                    if obj["name"] in self.object_data:
-                        if self.object_data[obj["name"]]["num_submit"] < self.object_data[obj["name"]]["num_object"]:
-                            if "location" in self.object_data[obj["name"]]:
-                                self.run.localize(obj['name'],
-                                        [self.object_data[obj["name"]]["location"]['x'], self.object_data[obj["name"]]["location"]['y']])
-                            else:
-                                self.run.localize(obj['name'],
-                                        [obj['x'], obj['y']])
-                            self.object_data[obj["name"]]["num_submit"] += 1
-                    else:
-                        self.run.localize(obj['name'],
-                                [obj['x'], obj['y']])  # submit localization attempt for drone at given [x,y] coordinates
-                if obj["name"] == "shipping-box":
-                    print("shipping box company at x: {}, y: {}: {}".format(obj['x'], obj['y'], potential_shipping[0]))
-        for name in self.object_data:
-            if "location" in self.object_data[name] and self.object_data[name]["num_submit"] < self.object_data[name]["num_object"]:
+            for obj_name in self.objects:
+                for obj in self.objects[obj_name]:
+                    if obj["count"] > min_count:
+                        if obj_name in self.given_object_data:
+                            if self.given_object_data[obj_name]["num_submit"] < self.given_object_data[obj_name]["num_object"]:
+                                if "location" in self.given_object_data[obj_name]:
+                                    self.run.localize(obj_name,
+                                            [self.given_object_data[obj_name]["location"]['x'], self.given_object_data[obj_name]["location"]['y']])
+                                else:
+                                    self.run.localize(obj_name,
+                                            [obj['x'], obj['y']])
+                                self.given_object_data[obj_name]["num_submit"] += 1
+                        else:
+                            self.run.localize(obj_name,
+                                    [obj['x'], obj['y']])  # submit localization attempt for drone at given [x,y] coordinates
+                    if obj_name == "shipping-box":
+                        print("shipping box company at x: {}, y: {}: {}".format(obj['x'], obj['y'], potential_shipping[0]))
+        for name in self.given_object_data:
+            if "location" in self.given_object_data[name] and self.given_object_data[name]["num_submit"] < self.given_object_data[name]["num_object"]:
                 self.run.localize(name,
-                                [self.object_data[name]["location"]['x'], self.object_data[name]["location"]['y']])
-                self.object_data[name]["num_submit"] += 1
+                                [self.given_object_data[name]["location"]['x'], self.given_object_data[name]["location"]['y']])
+                self.given_object_data[name]["num_submit"] += 1
         self.run.stop()  # stop the run
         # self.run.status # returns one of 'init', 'started', 'stopped'
         # self.run.events # returns a list of all recorded localization attempt events for this run
@@ -299,6 +308,3 @@ state = State(sched, simulated, run_number, try_legit, auto_submit_minutes)
 #                                      allowed_trans_error=0.3, allowed_rot_error=0.5)
 state.control_loop()
 state.end()
-# for i in range(100):
-#     state.sched.stop_action(i)
-# state.delete_state()
